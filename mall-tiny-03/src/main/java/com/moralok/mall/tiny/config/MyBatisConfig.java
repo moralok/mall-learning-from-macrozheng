7b11fcb9cc0336ca460b79ec57edcd78051f187d
moralok <moralok@163.com>
2019-07-31 23:58:38 +0800

    整合 Redis 并实现验证码功能

---
+++ mall-tiny-03/src/main/java/com/moralok/mall/tiny/config/MyBatisConfig.java
@@ -0,0 +1,15 @@
+package com.moralok.mall.tiny.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ *
+ * @author moralok
+ * @date 2019/7/31
+ */
+@Configuration
+@MapperScan("com.moralok.mall.tiny.mbg.mapper")
+public class MyBatisConfig {
+}
