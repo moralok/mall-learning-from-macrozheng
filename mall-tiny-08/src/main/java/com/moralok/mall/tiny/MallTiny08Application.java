a1b60100289036d4649b517d9a2daca221ccb293
moralok <moralok@163.com>
2019-08-02 01:30:03 +0800

    初始化项目模块8

---
+++ mall-tiny-08/src/main/java/com/moralok/mall/tiny/MallTiny08Application.java
@@ -0,0 +1,13 @@
+package com.moralok.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTiny08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTiny08Application.class, args);
+    }
+
+}
