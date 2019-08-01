c9662ab6639830e178488b94b6a9d005e5cc1d40
moralok <moralok@163.com>
2019-08-01 17:13:02 +0800

    Spring Security 和 JWT 实现认证

---
+++ mall-tiny-04/src/main/java/com/moralok/mall/tiny/dao/UmsAdminRoleRelationDao.java
@@ -0,0 +1,20 @@
+package com.moralok.mall.tiny.dao;
+
+import com.moralok.mall.tiny.mbg.model.UmsPermission;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 后台用户与角色管理自定义Dao
+ *
+ * @author moralok
+ * @date 2019/8/1
+ */
+public interface UmsAdminRoleRelationDao {
+
+    /**
+     * 获取用户所有权限(包括+-权限)
+     */
+    List<UmsPermission> getPermissionList(@Param("adminId") Long adminId);
+}
